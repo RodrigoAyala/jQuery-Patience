c1497051b1b65ec18cdbcccf15e20cf14d753ca8
Rodrigo Ayala <rayalac@yahoo.com>
2011-11-28 10:27:44 -0300

    Lastest modifications

--- jquery-patience.js
+++ jquery-patience.js
@@ -1,26 +1,51 @@
 try{
   (function( $ ){
+    var scripts = document.getElementsByTagName('script');
+    var thisScript = scripts[scripts.length-1];
+    var path=thisScript.src.substr(0,thisScript.src.lastIndexOf('/')+1);
 
     $.patience = function( options ) {  
 
       var settings = $.extend( {
         div_loading : 'loading-patience',
+        loading_image : path+"ajax-loader.gif",
         message : 'Loading...',
-        height: 200,
+        height: 120,
         width: 200
 
       }, options);
 
-      if (typeof options === "undefined" )
-        $('body').append('<div id="'+settings.div_loading+'" style="width:'+settings.width+'px;height:'+settings.height+'px; display:none;">'+settings.message+'</div>')
-      else
-        if (typeof options.div_loading === "undefined")
-        $('body').append('<div id="'+settings.div_loading+'" style="width:'+settings.width+'px;height:'+settings.height+'px; display:none;">'+settings.message+'</div>')
+      var div_loading = '<div id="'+settings.div_loading+'" style="width:'+settings.width+'px;height:'+settings.height+'px; display:none; background-image:url('+settings.loading_image+'); background-repeat:no-repeat; background-position: center 30%; border:1px solid; box-shadow:2px 2px 5px #888888; text-align:center; position:absolute; margin-left:auto; margin-right:auto"><div style="margin-top:'+(settings.height-120+85)+'px;">'+settings.message+'</div></div>'
 
+      if (typeof options === "undefined" ){
+        $('#'+settings.div_loading).remove();
+        $('body').append(div_loading);
+
+      } else{
+        if (typeof options.div_loading === "undefined"){
+          $('#'+settings.div_loading).remove();
+          $('body').append(div_loading);
+        }
+      }
+
+      $('#'+settings.div_loading).ajaxStart(function(){
+
+        var v_position = $(window).scrollTop()+ (($(window).height()-$(this).height())/2)
+        var h_position = $(window).scrollLeft()+ (($(window).width()-$(this).width())/2)
+
+        $(this).css('top',v_position);
+        $(this).css('left',h_position);
+        $(this).show();
+
+      });
+
+      $('#'+settings.div_loading).ajaxStop(function(){
+        $(this).hide();
+
+      });
     };
   })( jQuery );
 } catch (err)
-
 {
   if (err.toString().match('jQuery is not defined') != null)
     alert('I\'m sorry, but jQuery is not loaded')
@@ -29,5 +54,5 @@ try{
 $(document).ready(function(){
   if ($('meta[name=patience]').attr('content') === "true"){
     $.patience();
-    }
+  }
 });
